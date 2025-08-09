311a928931476f6e4eb11f59dde21943e38d28f0
kj7342 <kj7342@gmail.com>
2025-08-08 20:27:28 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bt-v2.0.3';
+const CACHE = 'bt-v2.0.4';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./db.js','./manifest.webmanifest','./auth.js','./cloudkit-sync.js','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); });
