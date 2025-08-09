a7421b57954f37a23b629bb99948bc2ef425a2cf
kj7342 <kj7342@gmail.com>
2025-08-08 21:51:57 -0400

    Auto refresh on service worker update

--- sw.js
+++ sw.js
@@ -3,9 +3,22 @@ const ASSETS = [
   './','./index.html','./styles.css','./app.js','./db.js','./faceid.js','./manifest.webmanifest',
   './icons/icon-192.png','./icons/icon-512.png'
 ];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); });
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+});
+
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
-  if (url.origin === location.origin) e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
 });
